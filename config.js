72badfc841e6a56d48ff02abd1d0c087d26faeea
sandseakrabi <sandseakrabi@gmail.com>
2026-07-24 13:02:12 +0700

    config.js

--- config.js
+++ config.js
@@ -12,4 +12,4 @@
  * 2. เอา URL นั้นมาวางแทนค่าด้านล่างนี้ (อยู่ในเครื่องหมายคำพูดเดิม)
  */
 
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwRm5E7-LIQBs5_ja7KaKrpozlg_NCyul6fHmf-JgcK0Y0LiXoJM5tEMv7VF40Y7LTo/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxY9L3zcdwt9wxhODkvnftaJBR4-aakWusduGy7jfnoV_MN4lcB2Rx6wZh_y4TgdSfe/exec';
